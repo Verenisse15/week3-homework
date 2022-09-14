c55286692e37e015912d95d9cffa2f7028e97d63
Verenisse15 <veryy0206@gmail.com>
2022-09-13 19:54:03 -0500

    completed

--- week3-coding.js
+++ week3-coding.js
@@ -40,10 +40,17 @@ for(let i = 0; i < names.length; i++) {
 console.log(concatenatedNames);
 
 //How do you access the last element of any array?
-//A: You can access the last element of any array by counting from left to right starting from zero. The last element whatever number it falls on will be it.
+//A: You can access the last element of any array by counting from left to right starting from zero. The last element whatever number it falls on will be it. You will need to type in items[items.length-1].
+let list = ['water', 'milk', 'soda']
+console.log(list)
+console.log(list.at(2))
+
 
 //How do you access the first element of any array?
-//A: 
+//A: To access first element of any array you can type item [0].  
+let myList = [ 1, 2, 3]
+console.log(myList)
+console.log(myList.at(0))
 
 // Create a new array called nameLengths.
 let nameLengths = [];
@@ -68,10 +75,18 @@ for( let i = 0 ; i < nameLengths.length; i++){
 console.log(total);
 
 //Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in ‘Hello’ and 3, I would expect the function to return ‘HelloHelloHello’).
-function createWord( word, n){
-   
-    return 
+let myWord = ["Hello", 3];
+function repeatWordThreeTimes(word, n){
+
+    let myWordRepeatTimesThree = 'Hello';
+    for(let i = 0; i < word.length; i++){
+        console.log(repeatWordThreeTimes[i]);
+        myWordRepeatTimesThree = myWordRepeatTimesThree + word[i];
+    }
+    console.log(myWordRepeatTimesThree);
 }
+console.log("Hello".repeat(3));
+
 //Write a function that takes two parameters, firstName and lastName, and returns a full name (the full name should be the first and the last name separated by a space).
 function createFullName (firstName, lastName){
     createFullName ("firstName" +' '+ "lastName");
@@ -80,7 +95,7 @@ function createFullName (firstName, lastName){
 console.log("Verenisse Ledesma");
 
 //Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
-let myNumbers = [50, 20, 40, 10];
+let myNumbers = [1, 6, 40, 10];
 function myTotal(array){ 
 
     let sumOfNumbers = 0;
@@ -99,24 +114,45 @@ console.log(myTotal(myNumbers));
 
 
 //Write a function that takes an array of numbers and returns the average of all the elements in the array.
-let myElements = [ 8, 20, 7, 1];
-function array(average){
+let myElements = [10, 20, 1, 100];
+function array(averageNum){
     
-    let averageNumber = 0;
-    for ( let i = 0; i < average.length; i ++){
-        console.log(average[i]);
-        averageAge = averageAge + average[i];
+    let array = " ";
+    
+    for ( let i = 0; i < averageNum.length; i ++){
+        console.log(averageNum[i]);
+        console.log(averageNum[i].length);
+        averageNum = averageNum + array[i].length;
     }
-    console.log(averageNumber);
-    return averageNumber; 
+    console.log(myElements)
+    let average = myElements / averageNum.length;
 }
+console.log(average);
 
 //Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second arra
-
-
-//Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
- 
-
-
-
+let theArrayNum = [20, 21]
+function myTwoArrays(arr1, arr2) {
+    if ( arr1 > arr2){
+        return true;
+    }else{
+        return false;
+    }
+}console.log(myTwoArrays(theArrayNum));
+//Write a function calld willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+let isHotOutside = true
+let moneyInPocket = 11
+    if ( isHotOutside && moneyInPocket >= 10.50){
+        console.log(true);
+    } else {
+        console.log(false);
+    }
 //Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
+// Write a function called additionOfNum with three parameters. Add all three to get the result.
+let a = 5
+let b = 10
+let c = 15
+function additionOfNum(a, b, c){
+    return a + b + c; 
+}
+console.log(additionOfNum(a, b, c));
+// I decided to make this quick and easy function of adding three numbers and return the total to the console.
